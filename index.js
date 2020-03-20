2f703dcba621b2b854e4765db41b5956ed8ca9ae
Colin Folta <colin.folta@gmail.com>
2020-03-20 14:51:14 -0500

    add message queue spy

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+// view messages being sent across bridge
+import MessageQueue from 'react-native/Libraries/BatchedBridge/MessageQueue';
+MessageQueue.spy(true);
+
 AppRegistry.registerComponent(appName, () => App);
